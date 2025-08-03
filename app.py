a9c07f51286fb4bf4c99a2491ad94855d6eaefb0
kartikj69 <jkartik156@gmail.com>
2025-08-03 16:44:51 +0530

    changed port

--- app.py
+++ app.py
@@ -430,4 +430,4 @@ def hello_world():
                                 current_date=current_date)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5050)
+    app.run(debug=True, host='0.0.0.0', port=5090)
